f27341c56b5e62b0e688fc3790723ddc98714487
WizMager <wizmager@gmail.com>
2022-10-11 21:10:52 +0300

    feat: init commit

---
+++ Assets/Scripts/CubeSpawner.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class CubeSpawner : MonoBehaviour
+{
+        [SerializeField] private GameObject cubePrefab;
+        [SerializeField] private Slider spawnSlider;
+        [SerializeField] private Slider speedSlider;
+        [SerializeField] private Slider distanceSlider;
+        
+}
---
+++ Assets/Scripts/Pool/CubePool.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Pool
+{
+    public class CubePool
+    {
+        private readonly GameObject _cubePrefab;
+        private readonly Stack<Cube> _storage;
+        private readonly Transform _rooTransform;
+
+        public CubePool(GameObject cubePrefab, int storageAmount)
+        {
+            _cubePrefab = cubePrefab;
+            _storage = new Stack<Cube>(storageAmount);
+            _rooTransform = new GameObject("PoolRoot").GetComponent<Transform>();
+            FillPool(storageAmount);
+        }
+
+        public void Push(Cube cube)
+        {
+            cube.gameObject.SetActive(false);
+            _storage.Push(cube);
+        }
+
+        public Cube Pop()
+        {
+            Cube cube;
+            if (_storage.Count > 0)
+            {
+                cube = _storage.Pop();
+                cube.gameObject.SetActive(true);
+            }
+            else
+            {
+                cube = Create();
+            }
+
+            return cube;
+        }
+
+        private Cube Create()
+        {
+            var cubeGameObject = Object.Instantiate(_cubePrefab, _rooTransform);
+            var cube = cubeGameObject.GetComponent<Cube>();
+            return cube;
+        }
+
+        private void FillPool(int storageAmount)
+        {
+            for (int i = 0; i < storageAmount; i++)
+            {
+                var cube = Create();
+                cube.gameObject.SetActive(false);
+                Push(cube); 
+            }
+        }
+    }
+}
