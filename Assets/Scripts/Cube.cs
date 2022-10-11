9e2f7177ba32aa8a866794f74d741dd688c99e23
WizMager <wizmager@gmail.com>
2022-10-11 22:30:16 +0300

    feat: add cube pool ref: rework UI elements feat: cube move with set speed and on set distance feat: user can change speed, moveDistance and spawn cooldown in Ui

--- Assets/Scripts/Cube.cs
+++ Assets/Scripts/Cube.cs
@@ -1,6 +1,30 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections;
+using UnityEngine;
 
 public class Cube : MonoBehaviour
 {
+        public Action<Cube> OnFinishMove;
+        [SerializeField] private Transform cubeTransform;
+        private float _moveSpeed;
+        private float _moveDistance;
+
+        public void StartCube(float moveSpeed, float moveDistance)
+        {
+                _moveSpeed = moveSpeed;
+                _moveDistance = moveDistance;
+                StartCoroutine(Move());
+        }
         
+        private IEnumerator Move()
+        {
+                for (float i = 0; i < _moveDistance; )
+                {
+                        var frameMoveDistance = Time.deltaTime * _moveSpeed;
+                        i += frameMoveDistance;
+                        cubeTransform.Translate(cubeTransform.forward * frameMoveDistance);
+                        yield return null;
+                }
+                OnFinishMove?.Invoke(this);
+        }
 }
---
+++ Assets/Scripts/CubeController.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using Pool;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CubeController : MonoBehaviour
+{
+        [SerializeField] private GameObject cubePrefab;
+        [SerializeField] private int cubePoolStartStorage;
+        [SerializeField] private Transform spawnPosition;
+        [SerializeField] private Slider spawnSlider;
+        [SerializeField] private Slider speedSlider;
+        [SerializeField] private Slider distanceSlider;
+        private CubePool _cubePool;
+        private float _spawnCooldown;
+        private float _moveSpeed;
+        private float _moveDistance;
+
+        private void Start()
+        {
+            _cubePool = new CubePool(cubePrefab, cubePoolStartStorage);
+            _spawnCooldown = spawnSlider.value;
+            _moveSpeed = speedSlider.value;
+            _moveDistance = distanceSlider.value;
+            spawnSlider.onValueChanged.AddListener(OnSpawnCooldownChangeHandler);
+            speedSlider.onValueChanged.AddListener(OnSpeedChangeHandler);
+            distanceSlider.onValueChanged.AddListener(OnDistanceChangeHandler);
+            StartCoroutine(SpawnCube());
+        }
+
+        private void OnSpawnCooldownChangeHandler(float newValue)
+        {
+            _spawnCooldown = newValue;
+        }
+
+        private void OnSpeedChangeHandler(float newValue)
+        {
+            _moveSpeed = newValue;
+        }
+
+        private void OnDistanceChangeHandler(float newValue)
+        {
+            _moveDistance = newValue;
+        }
+
+        private IEnumerator SpawnCube()
+        {
+            for (float i = 0; i < _spawnCooldown; i += Time.deltaTime)
+            {
+                yield return null;
+            }
+            SpawnCubeSetup();
+        }
+
+        private void SpawnCubeSetup()
+        {
+            var cubeComponent = _cubePool.Pop();
+            cubeComponent.gameObject.transform.position = spawnPosition.position;
+            cubeComponent.OnFinishMove += OnFinishCubeMoveHandler;
+            cubeComponent.StartCube(_moveSpeed, _moveDistance);
+            StartCoroutine(SpawnCube());
+        }
+
+        private void OnFinishCubeMoveHandler(Cube cubeComponent)
+        {
+            cubeComponent.OnFinishMove -= OnFinishCubeMoveHandler;
+            _cubePool.Push(cubeComponent);
+        }
+}
