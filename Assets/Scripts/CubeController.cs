24674a311335c90f25f3adb0f8b189f351a797f5
WizMager <wizmager@gmail.com>
2022-10-12 00:25:06 +0300

    ref: change input value of speed, distance and spawn from slider to input field text(like in task) feat: add wrong input protection

--- Assets/Scripts/CubeController.cs
+++ Assets/Scripts/CubeController.cs
@@ -1,48 +1,123 @@
 ﻿using System.Collections;
+using System.Globalization;
 using Pool;
+using TMPro;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class CubeController : MonoBehaviour
 {
         [SerializeField] private GameObject cubePrefab;
         [SerializeField] private int cubePoolStartStorage;
         [SerializeField] private Transform spawnPosition;
-        [SerializeField] private Slider spawnSlider;
-        [SerializeField] private Slider speedSlider;
-        [SerializeField] private Slider distanceSlider;
+        [SerializeField] private TMP_InputField spawnInputField;
+        [SerializeField] private TMP_InputField speedInputField;
+        [SerializeField] private TMP_InputField distanceInputField;
+        [SerializeField] private TMP_Text currentSpeedText;
+        [SerializeField] private TMP_Text currentDistanceText;
+        [SerializeField] private TMP_Text currentSpawnCooldownText;
         private CubePool _cubePool;
-        private float _spawnCooldown;
-        private float _moveSpeed;
-        private float _moveDistance;
+        private float _spawnCooldown = 1;
+        private float _moveSpeed = 1;
+        private float _moveDistance = 1;
 
         private void Start()
         {
+            currentSpawnCooldownText.text = $"Current cooldown: {_spawnCooldown}";
+            currentSpeedText.text = $"Current speed: {_moveSpeed}";
+            currentDistanceText.text = $"Current distance: {_moveDistance}";
             _cubePool = new CubePool(cubePrefab, cubePoolStartStorage);
-            _spawnCooldown = spawnSlider.value;
-            _moveSpeed = speedSlider.value;
-            _moveDistance = distanceSlider.value;
-            spawnSlider.onValueChanged.AddListener(OnSpawnCooldownChangeHandler);
-            speedSlider.onValueChanged.AddListener(OnSpeedChangeHandler);
-            distanceSlider.onValueChanged.AddListener(OnDistanceChangeHandler);
+            spawnInputField.onEndEdit.AddListener(OnSpawnCooldownChangeHandler);
+            speedInputField.onEndEdit.AddListener(OnSpeedChangeHandler);
+            distanceInputField.onEndEdit.AddListener(OnDistanceChangeHandler);
             StartCoroutine(SpawnCube());
         }
 
-        private void OnSpawnCooldownChangeHandler(float newValue)
+        private void OnSpawnCooldownChangeHandler(string newValue)
         {
-            _spawnCooldown = newValue;
+            if (InputFieldValueChecker(newValue))
+            {
+                _spawnCooldown = float.Parse(ReplaceToDot(newValue), CultureInfo.InvariantCulture);
+                if (_spawnCooldown <= 0)
+                {
+                    _spawnCooldown = 0.1f;
+                }
+                spawnInputField.text = "";
+                currentSpawnCooldownText.text = $"Current cooldown: {newValue}";
+            }
+            else
+            {
+                spawnInputField.text = "";
+            }
         }
 
-        private void OnSpeedChangeHandler(float newValue)
+        private void OnSpeedChangeHandler(string newValue)
         {
-            _moveSpeed = newValue;
+            
+            if (InputFieldValueChecker(newValue))
+            {
+                _moveSpeed = float.Parse(ReplaceToDot(newValue), CultureInfo.InvariantCulture);
+                if (_moveSpeed <= 0)
+                {
+                    _moveSpeed = 0.1f;
+                }
+                speedInputField.text = "";
+                currentSpeedText.text = $"Current speed: {newValue}";
+            }
+            else
+            {
+                speedInputField.text = "";
+            }
         }
 
-        private void OnDistanceChangeHandler(float newValue)
+        private void OnDistanceChangeHandler(string newValue)
         {
-            _moveDistance = newValue;
+            if (InputFieldValueChecker(newValue))
+            {
+                _moveDistance = float.Parse(ReplaceToDot(newValue), CultureInfo.InvariantCulture);
+                if (_moveDistance <= 0)
+                {
+                    _moveDistance = 0.1f;
+                }
+                distanceInputField.text = "";
+                currentDistanceText.text = $"Current distance: {newValue}";
+            }
+            else
+            {
+                distanceInputField.text = "";
+            }
         }
 
+        private bool InputFieldValueChecker(string newValue)
+        {
+            if (string.IsNullOrWhiteSpace(newValue))
+            {
+                return false;
+            }
+            foreach (var value in newValue.ToCharArray())
+            {
+                if (value >= '0' && value <= '9' || value == '.' || value == ',')
+                {
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private string ReplaceToDot(string value)
+        {
+            var newValueChar = value.ToCharArray();
+            for (int i = 0; i < newValueChar.Length; i++)
+            {
+                if (newValueChar[i] != ',') continue;
+                newValueChar[i] = '.';
+                break;
+            }
+            return new string(newValueChar);
+        }
+        
         private IEnumerator SpawnCube()
         {
             for (float i = 0; i < _spawnCooldown; i += Time.deltaTime)
@@ -65,5 +140,12 @@ public class CubeController : MonoBehaviour
         {
             cubeComponent.OnFinishMove -= OnFinishCubeMoveHandler;
             _cubePool.Push(cubeComponent);
+        }
+
+        private void OnDestroy()
+        {
+            spawnInputField.onEndEdit.RemoveListener(OnSpawnCooldownChangeHandler);
+            speedInputField.onEndEdit.RemoveListener(OnSpeedChangeHandler);
+            distanceInputField.onEndEdit.RemoveListener(OnDistanceChangeHandler);
         }
 }
